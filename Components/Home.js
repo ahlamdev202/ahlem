75fbdf9f3e97a027f5a3bc39f39e414d98b6525f
ahlamdev202 <129214695+ahlamdev202@users.noreply.github.com>
2023-03-28 18:08:14 +0000

    Add files via upload

---
+++ Components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>hello its me Ahlem</h1>
+    </div>
+  )
+}
+
+export default Home
---
+++ Components/MyWork.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyWork = () => {
+  return (
+    <div>
+      <h1>here is my Work</h1>
+    </div>
+  )
+}
+
+export default MyWork
